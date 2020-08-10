eff698586a6c1b21dda7380d254e886565c43637
Jacob Viti <thevitij64@gmail.com>
2020-08-10 03:40:31 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,7 +1,7 @@
 class Triangle
    attr_accessor :sone, :stwo, :sthree
 
-  def initialize (sone, stwo, sthree)
+  def initialize(sone, stwo, sthree)
   @sone = sone
   @stwo = stwo
   @sthree = sthree
